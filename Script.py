01ffe9e530cbe70a9e6cb645059cbbb35e7a904b
woslyricz <woslyricz@gmail.com>
2026-01-01 22:46:43 +0530

    Update greeting text in Script.py

--- Script.py
+++ Script.py
@@ -1,5 +1,5 @@
 class script(object):
-    START_TXT = """<b><u>🚩 ᴊᴀɪ ꜱʜʀɪ ʀᴀᴍ 🚩</u></b>
+    START_TXT = """<b><u>🚩 ʜᴇʏ ᴛʜᴇʀᴇ 🚩</u></b>
 
 <b>ʜᴇʏ {}, {}</b>
 
@@ -7,7 +7,7 @@ class script(object):
 """
 
 
-    GSTART_TXT = """<b>🚩 ᴊᴀɪ ꜱʜʀɪ ʀᴀᴍ 🚩</b>
+    GSTART_TXT = """<b>🚩 ʜᴇʏ ᴛʜᴇʀᴇ 🚩</b>
 
 <b>ʜᴇʏ {},</b>
 
